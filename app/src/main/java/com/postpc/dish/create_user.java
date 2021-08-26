17714e5e63e36fe9d4407959545da2ec29883368
Gil Rosenberg <gil.rosenberg@mail.huji.ac.il>
2021-08-26 14:36:44 +0300

    create_user latest changes

--- app/src/main/java/com/postpc/dish/create_user.java
+++ app/src/main/java/com/postpc/dish/create_user.java
@@ -1,5 +1,6 @@
 package com.postpc.dish;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,14 +8,21 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
 public class create_user extends Fragment {
 
     private CreateUserViewModel mViewModel;
+    private GoogleSignInClient mGoogleSignInClient;
+    private final static int RC_SIGN_IN = 123;
 
     public static create_user newInstance() {
         return new create_user();
@@ -29,17 +37,48 @@ public class create_user extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        view.findViewById(R.id.search_resturant_button).setOnClickListener(new View.OnClickListener() {
+        AppCompatButton signInButton = view.findViewById(R.id.google_signIn);
+
+        createRequest();
+        signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-
-                NavHostFragment navHostFragment = (NavHostFragment) requireActivity().getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
-                NavController navController = navHostFragment.getNavController();
-                navController.navigate(R.id.action_create_user_to_search_resturants2);
-
+            public void onClick(View view) {
+                signIn();
             }
         });
+
+//        view.findViewById(R.id.search_resturant_button).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                NavHostFragment navHostFragment = (NavHostFragment) requireActivity().getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+//                NavController navController = navHostFragment.getNavController();
+//                navController.navigate(R.id.action_create_user_to_search_resturants2);
+//
+//            }
+        }
+
+    /**
+     * creating a request that will be send to Google
+     */
+    private void createRequest() {
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(requireActivity(), gso);
+
+
     }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
